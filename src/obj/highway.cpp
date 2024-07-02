1a7178f7a5d8439816e024aab69e48be3fd48051
Manulito <manuasjkl@gmail.com>
2024-07-02 20:40:49 -0300

    Revert "Revert "coso""
    This reverts commit abe48488299b58706b7e1ae0cafe6c4abffaba91.

--- src/obj/highway.cpp
+++ src/obj/highway.cpp
@@ -183,8 +183,8 @@ void Highway::loadTrigger(){
 	
 	// string to int, string to double (float)
 	mTriggerPos = atoi(sTriggerPos.c_str());
-	mTriggerTurnTarget = atof(sTriggerTurnTarget);
-	mTriggerTurnSpeed = atof(sTriggerTurnPos);
+	mTriggerTurnTarget = (float)atof(sTriggerTurnTarget.c_str());
+	mTriggerTurnSpeed = (float)atof(sTriggerTurnPos.c_str());
 	
 	// temp 
 	/*mTriggerTurnSpeed = 0.001;
